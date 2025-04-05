2151a89feb836f0633d306e576d9ad373b315b4e
IamSamk <kadam.samartha@yahoo.com>
2025-04-05 13:47:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -39,7 +38,7 @@ const App: React.FC = () => {
           <Sonner />
           <ApiKeyInput />
           <Chatbot />
-          <BrowserRouter>
+          <HashRouter>
             <div className="flex flex-col min-h-screen">
               <Navbar />
               <main className="flex-grow pt-16">
@@ -58,7 +57,7 @@ const App: React.FC = () => {
               </main>
               <Footer />
             </div>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </LanguageProvider>
     </QueryClientProvider>
